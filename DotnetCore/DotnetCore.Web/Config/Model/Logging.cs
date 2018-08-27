38de5056ad1ccc7888b166487fc1a7dcde05fe36
ext.thomerson.tang <ext.thomerson.tang@ef.com>
2018-08-27 15:09:02 +0800

    init

---
+++ DotnetCore/DotnetCore.Web/Config/Model/Logging.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetCore.Web
+{
+    public class Logging
+    {
+        public bool IncludeScopes { get; set; }
+
+        public LogLevel LogLevel { get; set; }
+    }
+
+    public class LogLevel
+    {
+        public string Default { get; set; }
+    }
+}
